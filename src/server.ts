ec7846b8e8753d9606937c6113c04f04d7af5315
Gabriela Tavares <gabriela.ttavares@gmail.com>
2022-12-03 13:05:53 -0300

    :heavy_check_mark: config: add db connection

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,26 @@
 import express from "express";
+import mongoose from "mongoose";
 import ongRouter from "./routes/index";
+require('dotenv').config()
 
 const app = express();
 app.use(express.json());
 app.use(ongRouter);
 
+const PORT = process.env.PORT || 3000;
 
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.iew3ykh.mongodb.net/?retryWrites=true&w=majority`
+    
+  )
+  .then((data) => {
+    console.log("MongoDB Connection Succeeded");
+  })
+  .catch((err) => {
+    console.log("Error in BD connection", err.message);
+  });
 
-app.listen(3001, () => console.log("Server running!"));
+app.listen(PORT, () => console.log("Server running!"));
+
+console.log('PORT', process.env.PORT)
