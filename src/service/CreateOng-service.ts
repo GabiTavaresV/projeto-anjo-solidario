5820d42efb8556b08416cbc7e6dae868ae9c524b
Gabriela Tavares <gabriela.ttavares@gmail.com>
2022-12-03 16:04:07 -0300

    :white_check_mark: feat: add CreateOngService class

---
+++ src/service/CreateOng-service.ts
@@ -0,0 +1,25 @@
+import { ICreateOng } from "../interfaces/createOng";
+import { MongoRepository } from "../repository/repository";
+
+export class CreateOngService {
+  repository: MongoRepository;
+  constructor(repository: MongoRepository) {
+    this.repository = repository;
+  }
+
+  async createOng(createOng: ICreateOng): Promise<any> {
+    try {
+      if (createOng) {
+        const itemData = await this.repository.save(createOng);
+        return itemData;
+      }
+    } catch (err) {
+      throw Error("Create ong falied");
+    }
+  }
+
+  async listOrgs(): Promise<any> {
+    const resultList = await this.repository.findAll();
+    return resultList;
+  }
+}
