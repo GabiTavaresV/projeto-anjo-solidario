ecf7aaafefc4e06bc87094b613e720091a68c650
Gabriela Tavares <gabriela.ttavares@gmail.com>
2022-12-03 16:07:45 -0300

    :link: feat: add route file

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,11 +1,15 @@
-import { Router, Request, Response } from "express";
-
+import { Router } from "express";
+import { CreateOngController } from "../controller/CreateOng.controller";
+import { MongoRepository } from "../repository/repository";
+import { CreateOngService } from "../service/CreateOng-service";
 
 const ongRouter = Router();
 
+const mongoRepository = new MongoRepository();
+const service = new CreateOngService(mongoRepository);
+const controller = new CreateOngController(service);
 
-ongRouter.get("/", (req: Request, res: Response) => {
-    return res.send('Hello World')
-});
+ongRouter.post("/", (req, res) => controller.createOrg(req, res));
+ongRouter.get("/", (req, res) => controller.listOngs(res));
 
 export default ongRouter;
