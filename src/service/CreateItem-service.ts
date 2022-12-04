e0666aa84aa7ce8a06480e2f47f125ea9776a69f
Gabriela Tavares <gabriela.ttavares@gmail.com>
2022-12-04 13:53:23 -0300

    :heavy_check_mark: feat: add CreateItemService class

---
+++ src/service/CreateItem-service.ts
@@ -0,0 +1,25 @@
+import { ICreateItem } from "../interfaces/createItem";
+import { MongoRepositoryItem } from "../repository/repositoryItem";
+
+export class CreateItemService {
+  repository: MongoRepositoryItem;
+  constructor(repository: MongoRepositoryItem) {
+    this.repository = repository;
+  }
+
+  async createItem(createItem: ICreateItem): Promise<any> {
+    try {
+      if (createItem) {
+        const itemData = await this.repository.save(createItem);
+        return itemData;
+      }
+    } catch (err) {
+      throw Error("Create ong falied");
+    }
+  }
+
+  async listItens(): Promise<any> {
+    const resultList = await this.repository.findAll();
+    return resultList;
+  }
+}
